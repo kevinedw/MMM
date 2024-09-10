68797eb4b054e61efd936260fee6f85a36e4d75a
Kevin Edwards <168381420+kevinedw@users.noreply.github.com>
2024-09-09 21:01:39 -0600

    Update SQL-JoinTables_5.sql
    Pruned out dates and other identifiers

--- SQL-JoinTables_5.sql
+++ SQL-JoinTables_5.sql
@@ -5,37 +5,32 @@ SELECT CAST(DATEADD(WEEK,
 				DATEDIFF(WEEK, 0, Close_Date), 
 				0) as date) AS [DATE],   --cast 'as date' to force the data-type to 'date' and not 'datetime'
 	SUM(FirstGiftOnly) AS [donations]
-FROM [NewDonationsBeginningJan2021-sub5k]
+FROM [NewDonationsBeginning]
 GROUP BY CAST(DATEADD(WEEK, DATEDIFF(WEEK, 0, Close_Date), 0) as date)  -- cast as date to match the select cast
 
 -- DROP VIEW view_union_FB_AdsManager
 CREATE VIEW view_union_FB_AdsManager AS
 SELECT * 
-FROM [FB-AdsManagerReportApr5-2021-Dec31-2021]
+FROM [FB-AdsManagerReport]
 UNION
 SELECT *
-FROM [FB-AdsManagerReport-2022]
+FROM [FB-AdsManagerReport]
 UNION
 SELECT *
-FROM [FB-AdsManagerReport-2023]
+FROM [FB-AdsManagerReport2]
 UNION
 SELECT *
-FROM [FB-AdsManagerReport-2024-partial]
+FROM [FB-AdsManagerReport-4]
 
   --Combine Sprout Social yearly tables
 --DROP VIEW IF EXISTS [view_union_sprout]
 CREATE VIEW view_union_sprout AS
 SELECT * 
-FROM [Competitor Performance(Sprout)_2021]
+FROM [Competitor Performance(Sprout)-1]
 UNION
 SELECT *
-FROM [Competitor Performance(Sprout)_2022]
-UNION
-SELECT *
-FROM [Competitor Performance(Sprout)_2023]
-UNION
-SELECT *
-FROM [Competitor Performance(Sprout)_2024-partial]
+FROM [Competitor Performance(Sprout)_2]
+
 
   --ORGANIC FB
 --DROP VIEW IF EXISTS [view_fb_Org_weekly]
@@ -45,25 +40,7 @@ CREATE VIEW view_fb_Org_weekly AS
  FROM view_union_sprout
  WHERE (Profile LIKE '%company%') AND (Network like 'Facebook')
  GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, [Date]), 0)
-
-   --ORGANIC IG
---DROP VIEW IF EXISTS [view_ig_Org_weekly]
-CREATE VIEW view_ig_Org_weekly AS
- SELECT 
-  DATEADD(WEEK, DATEDIFF(WEEK, 0, [Date]), 0) [DATE], SUM(Audience) [ig_org_I]
- FROM view_union_sprout
- WHERE (Profile LIKE '%company%') AND (Network like 'Instagram')
- GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, [Date]), 0)
-
-    --ORGANIC X
---DROP VIEW IF EXISTS [view_x_Org_weekly]
-CREATE VIEW view_x_Org_weekly AS
- SELECT 
-  DATEADD(WEEK, DATEDIFF(WEEK, 0, [Date]), 0) [DATE], SUM(Audience) [x_org_I]
- FROM view_union_sprout
- WHERE (Profile LIKE '%company%') AND (Network like 'X')
- GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, [Date]), 0)
-
+	
  -- NEW DONATIONS
 --DROP VIEW IF EXISTS [new_donations_view]
 CREATE VIEW new_donations_view AS
@@ -71,7 +48,7 @@ CREATE VIEW new_donations_view AS
 				DATEDIFF(WEEK, 0, Close_Date), 
 				0) as date) AS [DATE],   --cast 'as date' to force the data-type to 'date' and not 'datetime'
 	Sum(FirstGiftOnly) AS [donations]
- FROM NewDonationBeginningJan2021
+ FROM NewDonationBeginning
  GROUP BY CAST(DATEADD(WEEK, DATEDIFF(WEEK, 0, Close_Date), 0) as date)  -- cast as date to match the select cast
 
  -- SUBSCRIBERS
@@ -79,15 +56,15 @@ CREATE VIEW new_donations_view AS
 CREATE VIEW subscriber_view AS
  SELECT DATEADD(WEEK, DATEDIFF(WEEK, 0, Created_Date), 0) [DATE], 
 		COUNT(Account_ID) [subscribers]
- FROM [Subscribers-2022-2024]
+ FROM [Subscribers]
  GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, Created_Date), 0)
 
  --AGENCY IMPRESSIONS
---DROP VIEW IF EXISTS [view_m_impr]
-CREATE VIEW view_m_impr AS
+--DROP VIEW IF EXISTS [view_ag_impr]
+CREATE VIEW view_ag_impr AS
  SELECT DATEADD(WEEK, DATEDIFF(WEEK, 0, Interaction_Date), 0) [DATE], 
 		SUM(Impressions) impr
- FROM [m_impressions_2022_2024]
+ FROM [ag_impressions]
  GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, Interaction_Date), 0)
  
   -- FACEBOOK
@@ -100,26 +77,6 @@ CREATE VIEW view_facebook_feed AS
  FROM [FB-AdsManagerReport_comp-facebook]
  WHERE Placement LIKE 'feed'
  GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, [DATE]), 0), Placement
- 
-  -- FACEBOOK
---DROP VIEW IF EXISTS [view_facebook_reels]
-CREATE VIEW view_facebook_reels AS
- SELECT DATEADD(WEEK, DATEDIFF(WEEK, 0, [DATE]), 0) AS [DATE], 
-	SUM(Amount_spent_USD) [fb_spend],  
-	SUM(Impressions) AS fb_impr
- FROM [FB-AdsManagerReport_comp-facebook]
- WHERE Placement LIKE '%reels%'
- GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, [DATE]), 0)
-
- -- FACEBOOK
---DROP VIEW IF EXISTS [view_facebook_video]
-CREATE VIEW view_facebook_video AS
- SELECT DATEADD(WEEK, DATEDIFF(WEEK, 0, [DATE]), 0) AS [DATE], 
-	SUM(Amount_spent_USD) [fb_spend],  
-	SUM(Impressions) AS fb_impr
- FROM [FB-AdsManagerReport_comp-facebook]
- WHERE Placement LIKE '%video%'
- GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, [DATE]), 0)
 
   -- INSTAGRAM
 --DROP VIEW IF EXISTS [view_instagram]
@@ -135,7 +92,7 @@ CREATE VIEW view_instagram AS
 CREATE VIEW view_comp_1 AS
  SELECT 
   DATEADD(WEEK, DATEDIFF(WEEK, 0, [Date]), 0) [DATE], SUM(Audience) [comp_1_I]
- FROM [Competitor Performance(SproutScl)Jan12023 Apr222024]
+ FROM [Competitor Performance(SproutScl)]
  WHERE (Profile LIKE '%COMP1%')
  GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, [Date]), 0)
 
@@ -144,17 +101,8 @@ CREATE VIEW view_comp_1 AS
 CREATE VIEW view_comp_2 AS
  SELECT 
   DATEADD(WEEK, DATEDIFF(WEEK, 0, [Date]), 0) [DATE], SUM(Audience) [comp_2_I]
- FROM [Competitor Performance(SproutScl)Jan12023 Apr222024]
+ FROM [Competitor Performance(SproutScl)]
  WHERE (Profile LIKE '%COMP2%')
- GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, [Date]), 0)
-
- -- COMPETION-3
---DROP VIEW IF EXISTS [view_comp_3]
-CREATE VIEW view_comp_3 AS
- SELECT 
-  DATEADD(WEEK, DATEDIFF(WEEK, 0, [Date]), 0) [DATE], SUM(Audience) [comp_3_I]
- FROM [Competitor Performance(SproutScl)Jan12023 Apr222024]
- WHERE (Profile LIKE '%COMP3%')
  GROUP BY DATEADD(WEEK, DATEDIFF(WEEK, 0, [Date]), 0)
 
 --Need to run DROP separatley first...
@@ -167,34 +115,24 @@ CREATE VIEW view_joined AS
 	COALESCE(view_m_impr.impr,0) as m_I,
 	COALESCE(view_facebook_feed.fb_spend,0) as fb_feed_S,
 	COALESCE(view_facebook_feed.fb_impr, 0) as fb_feed_I,
-	COALESCE(view_facebook_video.fb_spend,0) as fb_video_S,
-	COALESCE(view_facebook_video.fb_impr, 0) as fb_video_I,
-	COALESCE(view_facebook_reels.fb_spend,0) as fb_reels_S,
-	COALESCE(view_facebook_reels.fb_impr, 0) as fb_reels_I,
 	COALESCE(view_instagram.ig_spend,0) as instagram_S,
 	COALESCE(view_instagram.ig_impr, 0) as instagram_I,
 	COALESCE(radio_S.radio_S,0) as radio_S,
 	COALESCE(radio_I.radio_I,0) as radio_I,
 	COALESCE(pr_S.pr_spend,0) as pr_S,
-	COALESCE([Youtube_MMM_Jan2021-May2024].Cost,0) as youtube_S,  -- needs [] because of dash in the name
-	COALESCE([Youtube_MMM_Jan2021-May2024].Impr,0) as youtube_I,
-	COALESCE([GoogleSearch_MMM_Jan2021-May2024].Cost,0) as search_S,  -- needs [] because of dash in the name
-	COALESCE([GoogleSearch_MMM_Jan2021-May2024].Clicks,0) as search_clicks_P,
+	COALESCE([Youtube_MMM].Cost,0) as youtube_S,  -- needs [] because of dash in the name
+	COALESCE([Youtube_MMM].Impr,0) as youtube_I,
+	COALESCE([GoogleSearch].Cost,0) as search_S,  -- needs [] because of dash in the name
+	COALESCE([GoogleSearch_MMM].Clicks,0) as search_clicks_P,
 	COALESCE(view_comp_1.[comp_1_I],0) as comp_1_I,
 	COALESCE(view_comp_2.[comp_2_I],0) as comp_2_I,
-	COALESCE(view_comp_3.[comp_3_I],0) as comp_3_I,
 	COALESCE(subscriber_view.subscribers, 0) as subscriber,
 	COALESCE(view_fb_Org_weekly.fb_org_I,0) as fb_O_I,
 	COALESCE(view_ig_Org_weekly.ig_org_I,0) as ig_O_I,
-	COALESCE(view_x_Org_weekly.x_org_I,0) as x_O_I,
 	COALESCE([events].event,'') as event	-- Coalesce to replace all NULL with blank text
  FROM new_donations_view AS d				--Use internal view created in other query
  FULL JOIN view_facebook_feed 
 	ON d.DATE = view_facebook_feed.DATE
- FULL JOIN view_facebook_video 
-	ON d.DATE = view_facebook_video.DATE
- FULL JOIN view_facebook_reels 
-	ON d.DATE = view_facebook_reels.DATE
  FULL JOIN view_instagram 
 	ON d.DATE = view_instagram.DATE
  FULL JOIN  m_spend
@@ -207,10 +145,10 @@ CREATE VIEW view_joined AS
 	ON d.DATE = [radio_I].DATE
  FULL JOIN pr_S
 	ON d.DATE = [pr_S].DATE
- FULL JOIN [Youtube_MMM_Jan2021-May2024]				-- both _S and _I
-	ON d.DATE = [Youtube_MMM_Jan2021-May2024].DATE
- FULL JOIN [GoogleSearch_MMM_Jan2021-May2024]			-- both _S and _I
-	ON d.DATE = [GoogleSearch_MMM_Jan2021-May2024].[Week]
+ FULL JOIN [Youtube_MMM]				-- both _S and _I
+	ON d.DATE = [Youtube_MMM].DATE
+ FULL JOIN [GoogleSearch_MMM]			-- both _S and _I
+	ON d.DATE = [GoogleSearch_MMM].[Week]
  FULL JOIN [events]
 	ON d.DATE = [events].DATE
  FULL JOIN subscriber_view
@@ -219,8 +157,6 @@ CREATE VIEW view_joined AS
 	ON d.DATE = view_comp_1.DATE
  FULL JOIN view_comp_2
 	ON d.DATE = view_comp_2.DATE
- FULL JOIN view_comp_3
-	ON d.DATE = view_comp_3.DATE
  FULL JOIN view_fb_Org_weekly 
 	ON d.DATE = view_fb_Org_weekly.DATE
  FULL JOIN view_ig_Org_weekly 
@@ -232,6 +168,4 @@ CREATE VIEW view_joined AS
 SELECT *
 FROM view_joined
 ORDER BY DATE asc
-
-EXEC sp_help view_fb_impr
 
